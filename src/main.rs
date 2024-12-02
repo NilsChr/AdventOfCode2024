9323f1cd30f926a6c07375c7da0d542b5279dc9c
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2024-12-02 13:03:55 +0100

    removed comments

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
+
 mod day1;
 mod day2;
 mod util {
-    pub mod file_reader; // Declare the fileReader module
+    pub mod file_reader; 
 }
 
 use std::time::Instant;
 
 fn main() {
-    let start_time = Instant::now(); // Start the timer
-
+    let start_time = Instant::now();
     day2::start();
-
     let elapsed_time = start_time.elapsed();
     println!("Execution time: {:.2?}", elapsed_time);
 }
